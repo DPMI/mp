7809a5e652b52993aff423a7485b62713c55129c
David Sveningsson <ext@sidvind.com>
2011-04-13 16:16:12 +0000

    refactoring raw capture, so it also uses the push_packet function
    git-svn-id: svn://sidvind.com/project/BTH/NPLDP/mp/trunk@1615 8459abcf-8237-4014-9f1e-057f68ddaf13

--- capture.c
+++ capture.c
@@ -82,133 +82,95 @@ static int push_packet(struct CI* CI, write_head* whead, cap_head* head, const u
   return recipient;
 }
 
+static int fill_caphead(cap_head* head, struct timeval* tv, size_t bytes, const char* iface, const char* MAMPid){
+  head->ts.tv_sec   = tv->tv_sec;   // Store arrival time in seconds
+  head->ts.tv_psec  = tv->tv_usec; // Write timestamp in picosec
+  head->ts.tv_psec *= 1000000;
+  head->len         = bytes;
+  strncpy(head->nic, iface, 4);
+  strncpy(head->mampid, MAMPid, 8);
+  return 0;
+}
+
+static void wait_for_auth(){
+  while ( terminateThreads == 0 ){
+    if ( MAMPid ){
+      return;
+    }
+    sleep(1); /** @todo should use a pthread cond. variable */
+  }
+}
+
 /* This is the RAW_SOCKET capturer..   */ 
 /* Lots of problems, use with caution. */
 void* capture(void* ptr){
   struct CI* CI = (struct CI*)ptr;
-
-  //int sd;                       // the socket
-  //  char nicString[4];            // The CI identifier
-  //char* nic;                    // name of the network interface
-  //int id;                       // number of this thread, used for memory access
-  cap_head *head;               // pointer cap_head
-  write_head *whead;            // pointer write_head
-  size_t buffsize=PKT_CAPSIZE;  // This is how much we extract from the network.
-  int writePos=0;               // Position in memory where to write
-  int packet_len=0;             // acctual length of packet
-  struct timeval time;          // arrivaltime
-  int consumer;                 // consumer identification.
-
-  //nicString[0]=cProc->nic[0];   // [e]th0\0
-  //nicString[1]=cProc->nic[3];   // eth[0]\0
-  //nicString[2]=0;
-  //nicString[3]=0;
-  //nic=nicString;
-
-  printf("CAPt: here.\n");
-  //sem_t* semaphore=cProc->semaphore;
-  //id=cProc->id;
+  CI->writepos = 0; /* Reset write-position in memory */
+  //  myTD=cProc->accuracy;
   
-  logmsg(verbose, "Capture for %s initializing, Memory at %p.\n", CI->nic, &datamem[CI->id]);
+  logmsg(verbose, "CI[%d] initializing capture on %s using RAW_SOCKET (memory at %p).\n", CI->id, CI->nic, &datamem[CI->id]);
 
-  struct timeval timeout;
-  fd_set fds;
-  timeout.tv_sec=5;
-  timeout.tv_usec=0;
-  FD_ZERO(&fds);
-  FD_SET(CI->sd, &fds);
-  int selectReturn=0;
-  
-  while(terminateThreads==0)  {
-    fprintf(verbose, "CaptureThread %ld Pkts : %d \n",pthread_self(), recvPkts);
-    do { // read packet from interface
-
-      while( (selectReturn=select(CI->sd+1,&fds,NULL,NULL, &timeout))<=0 && terminateThreads==0){
-	if(terminateThreads!=0){
-	  perror("CAPTURE(RAW):Got a break signal.\n");
-	  break;
-	}
-	if(selectReturn==-1){
-	  perror("CAPTURE(RAW):Select error:");
-	  //EXIT SOMEHOW
-	  pthread_exit(NULL);
-	} else if(selectReturn==0){
-	  FD_SET(CI->sd, &fds);
-	  timeout.tv_sec=5;
-	  timeout.tv_usec=0;
-	}
-      }
-	  
-      packet_len = recvfrom(CI->sd, (&datamem[CI->id][writePos][(sizeof(write_head)+sizeof(cap_head))]),
-			    buffsize, MSG_TRUNC, NULL, NULL);
-    }while (packet_len == -1 && errno == EINTR && terminateThreads==0);
-    
-    if (packet_len == -1)    {
-      if (errno == EAGAIN)
-	fprintf(stderr,"CAPTURE_RAW[%s] ERROR Empty\n", CI->nic);
-      else
-        fprintf(stderr,"CAPTURE_RAW[%s] ERROR %d: %s\n", CI->nic, errno, strerror(errno));
-    }
-    else {
-//This could be a problem if a new packet arrivs before timestamp???
-      ioctl(CI->sd, SIOCGSTAMP, &time );//get time stamp associated with packet (C/O kernel)
-      recvPkts++;
-      CI->pktCnt++;
-
-// int filter(void* pkt); 0=> DROP PKT. n, send to recipient n.
-      whead=(write_head*)datamem[CI->id][writePos];
-      head=(cap_head*)&datamem[CI->id][writePos][sizeof(write_head)];
-      consumer=filter(CI->nic, &datamem[CI->id][writePos][(sizeof(write_head)+sizeof(cap_head))], head);
-      if(consumer>=0)
-      {
-	matchPkts++;
-//Mutex begin; prevent the reader from operating on the same chunk of memory.
-	pthread_mutex_lock( &mutex2 );
-	whead=(write_head*)datamem[CI->id][writePos];
-	head=(cap_head*)&datamem[CI->id][writePos][sizeof(write_head)];
-	head->ts.tv_sec=time.tv_sec;  // Store arrival time in seconds
-	head->ts.tv_psec=time.tv_usec;// Write timestamp in picosec
-	head->ts.tv_psec*=1000;
-	head->ts.tv_psec*=1000;
-	head->len=packet_len; // Store packet lenght in header.
-                              // head->caplen will set by the filter, when copying data to sender buffer.
-	/*head->tsAccuracy=myTD;*/
-	strncpy(head->nic, CI->nic,4);
-	strncpy(head->mampid, MAMPid,8);
-	
-	whead->free=whead->free+1; //marks the post that it has been written
-	whead->consumer=consumer;  //sets the recipient id.
-	
-	if(whead->free>1){ //Control buffer overrun
-          fprintf(stderr,"OVERWRITING: %ld @ %d for the %d time \n",pthread_self(),writePos,whead->free);
-	  fprintf(stderr,"CT: bufferUsage[%d]=%d\n", CI->id, CI->bufferUsage);
-	  memDropcount++;
-	}
-	pthread_mutex_unlock( &mutex2 );
-//Mutex end
-	packet_len=0;
-
-	if ( sem_post(CI->semaphore) != 0 ){
-	  fprintf(stderr, "sem_post() returned %d: %s\n", errno, strerror(errno));
-	}
-
-	writePos++;
-	CI->bufferUsage++;
-        if(writePos<PKT_BUFFER){
-        } else {
-	  writePos=0; //when all posts in datamem is written begin from 0 again
-        }
-      }
+  wait_for_auth();
+  while( terminateThreads==0 )  {
+   
+    /* wait until data is available on socket */
+    {
+      struct timeval timeout = {1, 0};
+      fd_set fds;
+      FD_ZERO(&fds);
+      FD_SET(CI->sd, &fds);
       
+      if ( select(CI->sd+1,&fds,NULL,NULL, &timeout) == -1 ){
+	switch ( errno ){
+	case EAGAIN:
+	case EINTR:
+	  continue;
+	  
+	default:
+	  logmsg(stderr, "select() failed with code %d: %s\n", errno, strerror(errno));
+	  abort();
+	}
+      }
     }
+
+    unsigned char* raw_buffer = datamem[CI->id][CI->writepos];
+    write_head* whead   = (write_head*)raw_buffer;
+    cap_head* head      = (cap_head*)(raw_buffer + sizeof(write_head));
+    unsigned char* packet_buffer = raw_buffer + sizeof(write_head) + sizeof(cap_head);
+
+    const ssize_t bytes = recvfrom(CI->sd, packet_buffer, PKT_CAPSIZE, MSG_TRUNC, NULL, NULL);
+
+    if ( bytes == -1 ){
+      if ( errno == EAGAIN ){
+	continue;
+      }
+      logmsg(stderr, "recvfrom() failed with code %d: %s\n", errno, strerror(errno));
+      abort();
+    }
+
+    //This could be a problem if a new packet arrivs before timestamp???
+    struct timeval time;
+    ioctl(CI->sd, SIOCGSTAMP, &time );//get time stamp associated with packet (C/O kernel)
+
+    fill_caphead(head, &time, bytes, CI->nic, MAMPid);
+
+    recvPkts++;
+    CI->pktCnt++;
+
+    /* return -1 when no filter matches */
+    if ( push_packet(CI, whead, head, packet_buffer) == -1 ){
+      continue;
+    }
+
+    matchPkts++;
   }
+
   // comes here when terminateThreads = 1
   fprintf(verbose, "Child %ld My work here is done %s.\n", pthread_self(), CI->nic);
   return(NULL) ;
 }
 
 /* This is the PCAP_SOCKET capturer..   */ 
-/* Lots of problems, use with caution. */
 void* pcap_capture(void* ptr){
   struct CI* CI = (struct CI*)ptr;
   CI->writepos = 0; /* Reset write-position in memory */
@@ -225,6 +187,7 @@ void* pcap_capture(void* ptr){
     exit (1);
   }
 
+  wait_for_auth();
   while(terminateThreads==0)  {
     const u_char* payload = pcap_next(descr, &pcaphead);
     if(payload==NULL) {
@@ -232,21 +195,15 @@ void* pcap_capture(void* ptr){
       exit(1);
     }
 
-    const size_t data_len = MIN(pcaphead.caplen, PKT_CAPSIZE);
-    const size_t padding = PKT_CAPSIZE - data_len;
-
     unsigned char* raw_buffer = datamem[CI->id][CI->writepos];
-
     write_head* whead   = (write_head*)raw_buffer;
     cap_head* head      = (cap_head*)(raw_buffer + sizeof(write_head));
     unsigned char* packet_buffer = raw_buffer + sizeof(write_head) + sizeof(cap_head);
 
-    head->ts.tv_sec   = pcaphead.ts.tv_sec;  // Store arrival time in seconds
-    head->ts.tv_psec  = pcaphead.ts.tv_usec;// Write timestamp in picosec
-    head->ts.tv_psec *= 1000000;
-    head->len = pcaphead.len; // Store packet lenght in header.
-                              // head->caplen will set by the filter, when copying data to sender buffer.
-    /*head->tsAccuracy=myTD; */
+    const size_t data_len = MIN(pcaphead.caplen, PKT_CAPSIZE);
+    const size_t padding = PKT_CAPSIZE - data_len;
+
+    fill_caphead(head, &pcaphead.ts, pcaphead.len, CI->nic, MAMPid);
 
     memcpy(packet_buffer, payload, data_len);
     memset(packet_buffer + data_len, 0, padding);
