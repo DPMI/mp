d729c5908eaa39d7324acdc61b2c247aa8cc40f2
David Sveningsson <ext@sidvind.com>
2011-06-17 16:17:38 +0000

    moving code to raw driver
    git-svn-id: svn://sidvind.com/project/BTH/NPLDP/mp/trunk@1856 8459abcf-8237-4014-9f1e-057f68ddaf13

--- capture/raw.c
+++ capture/raw.c
@@ -6,15 +6,77 @@
 #include "log.h"
 
 #include <string.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <linux/if_packet.h>
 
 struct raw_context {
   struct capture_context base;
   int socket;
 };
+
+//Sets Nic to promisc mode
+static void setpromisc(int sd, char* device)
+{
+  struct ifreq	ifr;
+  memset(&ifr, 0, sizeof(ifr));
+  strncpy(ifr.ifr_name, device, sizeof(ifr.ifr_name));
+
+  if(ioctl(sd, SIOCGIFFLAGS, &ifr)==-1)
+  {
+    printf("can't open flags");
+    exit(1);
+  }
+  if (ifr.ifr_flags & IFF_PROMISC)
+  {
+    return;
+  }
+  else
+  {
+    ifr.ifr_flags |= IFF_PROMISC;
+    if(ioctl(sd, SIOCSIFFLAGS, &ifr)==-1)
+    {
+     printf("can't enter promisc");
+     return;
+    }
+  }
+  return;
+}
+
+/**
+ * Get the right id for nic (ethX->interface index) Used for bind
+ * @return ID or -1 on errors (errno is raised)
+ */
+static int iface_get_id(int sd, const char *device) {
+  struct ifreq	ifr;
+  memset(&ifr, 0, sizeof(ifr));
+  strncpy(ifr.ifr_name, device, sizeof(ifr.ifr_name));
+  if (ioctl(sd, SIOCGIFINDEX, &ifr) == -1) {
+    return -1;
+  }
+
+  return ifr.ifr_ifindex;
+}
+
+//Bind socket to Interface
+static int iface_bind(int fd, int ifindex){
+  struct sockaddr_ll sll;
+  memset(&sll, 0, sizeof(sll));
+  sll.sll_family		= PF_PACKET;
+  sll.sll_ifindex		= ifindex;
+  sll.sll_protocol	= htons(ETH_P_ALL);
+
+  if (bind(fd, (struct sockaddr *) &sll, sizeof(sll)) == -1) {
+    fprintf(stderr, "bind: %d: %s", errno, strerror(errno));
+    return -1;
+  }
+  return 0;
+}
 
 static int read_packet_raw(struct raw_context* ctx, unsigned char* dst, struct timeval* timestamp){
   int sd = ctx->socket;
@@ -64,7 +126,15 @@ void* capture(void* ptr){
   
   /* initialize raw capture */
   logmsg(verbose, "CI[%d] initializing capture on %s using RAW_SOCKET (memory at %p).\n", CI->id, CI->iface, &datamem[CI->id]);
+
+  /* open socket */
+  CI->sd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
   cap.socket = CI->sd;
+
+  /* setup socket properties */
+  int ifindex = iface_get_id(CI->sd, CI->iface);
+  iface_bind(CI->sd, ifindex);
+  setpromisc(CI->sd, CI->iface);
 
   /* setup callbacks */
   cap.base.read_packet = (read_packet_callback)read_packet_raw;
--- main.c
+++ main.c
@@ -69,12 +69,6 @@ struct MAinfo MA;
 static void cleanup(int sig); // Runs when program terminates
 static int packet_stats(int sd, struct packet_stat *stat); // function for collecting statistics
 
-#ifdef HAVE_DRIVER_RAW
-static void setpromisc(int sd, char* device); // function for setting interfaces to listen on all traffic
-static int iface_get_id(int sd, const char *device); //comments in code
-static int iface_bind(int fd, int ifindex); //comments in code
-#endif /* HAVE_DRIVER_RAW */
-
 static void info(int sd);// function for presentating statistics
 
 static short int iflag=0;  // number of capture interfaces
@@ -373,14 +367,6 @@ static int setup_capture(){
 
     case DRIVER_RAW:
 #ifdef HAVE_DRIVER_RAW
-      {
-        int ifindex;
-	CI[i].sd = socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
-	ifindex=iface_get_id(CI[i].sd, CI[i].iface);
-	iface_bind(CI[i].sd, ifindex);
-	setpromisc(CI[i].sd, CI[i].iface);
-      }
-
       func = capture;
 #else /* HAVE_DRIVER_RAW */
 	fprintf(stderr, "This MP lacks support for raw packet capture (use libpcap or DAG instead or rebuild with --with-raw)\n");
@@ -627,68 +613,6 @@ static void info(int sd)
     (void)fprintf(stderr, "\t%d packets received by filter\n", stat.pkg_recv);
     (void)fprintf(stderr, "\t%d packets dropped by kernel\n", stat.pkg_drop);
 }
-
-#ifdef HAVE_DRIVER_RAW
-
-//Sets Nic to promisc mode
-static void setpromisc(int sd, char* device)
-{
-  struct ifreq	ifr;
-  memset(&ifr, 0, sizeof(ifr));
-  strncpy(ifr.ifr_name, device, sizeof(ifr.ifr_name));
-
-  if(ioctl(sd, SIOCGIFFLAGS, &ifr)==-1)
-  {
-    printf("can't open flags");
-    exit(1);
-  }
-  if (ifr.ifr_flags & IFF_PROMISC)
-  {
-    return;
-  }
-  else
-  {
-    ifr.ifr_flags |= IFF_PROMISC;
-    if(ioctl(sd, SIOCSIFFLAGS, &ifr)==-1)
-    {
-     printf("can't enter promisc");
-     return;
-    }
-  }
-  return;
-}
-
-/**
- * Get the right id for nic (ethX->interface index) Used for bind
- * @return ID or -1 on errors (errno is raised)
- */
-static int iface_get_id(int sd, const char *device) {
-  struct ifreq	ifr;
-  memset(&ifr, 0, sizeof(ifr));
-  strncpy(ifr.ifr_name, device, sizeof(ifr.ifr_name));
-  if (ioctl(sd, SIOCGIFINDEX, &ifr) == -1) {
-    return -1;
-  }
-
-  return ifr.ifr_ifindex;
-}
-
-//Bind socket to Interface
-static int iface_bind(int fd, int ifindex){
-  struct sockaddr_ll sll;
-  memset(&sll, 0, sizeof(sll));
-  sll.sll_family		= PF_PACKET;
-  sll.sll_ifindex		= ifindex;
-  sll.sll_protocol	= htons(ETH_P_ALL);
-
-  if (bind(fd, (struct sockaddr *) &sll, sizeof(sll)) == -1) {
-    fprintf(stderr, "bind: %d: %s", errno, strerror(errno));
-    return -1;
-  }
-  return 0;
-}
-
-#endif /* HAVE_DRIVER_RAW */
 
 //create statistics for Nic (pkgdrop)
 static int packet_stats(int sd, struct packet_stat *stats){
