1a35a7b7c31c9a8eaefb31477774abcd38532449
David Sveningsson <ext@sidvind.com>
2012-11-16 13:06:09 +0100

    using formatting function from libcap_utils

--- src/capture.c
+++ src/capture.c
@@ -50,138 +50,6 @@
 
 extern int show_packets;
 
-static void print_tcp(FILE* dst, const struct ip* ip, const struct tcphdr* tcp){
-	fprintf(dst, "TCP(HDR[%d]DATA[%0x]):\t [",4*tcp->doff, ntohs(ip->ip_len) - 4*tcp->doff - 4*ip->ip_hl);
-	if(tcp->syn) {
-		fprintf(dst, "S");
-	}
-	if(tcp->fin) {
-		fprintf(dst, "F");
-	}
-	if(tcp->ack) {
-		fprintf(dst, "A");
-	}
-	if(tcp->psh) {
-		fprintf(dst, "P");
-	}
-	if(tcp->urg) {
-		fprintf(dst, "U");
-	}
-	if(tcp->rst) {
-		fprintf(dst, "R");
-	}
-
-	fprintf(dst, "] %s:%d ",inet_ntoa(ip->ip_src),(u_int16_t)ntohs(tcp->source));
-	fprintf(dst, " --> %s:%d",inet_ntoa(ip->ip_dst),(u_int16_t)ntohs(tcp->dest));
-	fprintf(dst, "\n");
-}
-
-static void print_udp(FILE* dst, const struct ip* ip, const struct udphdr* udp){
-	fprintf(dst, "UDP(HDR[8]DATA[%d]):\t %s:%d ",(u_int16_t)(ntohs(udp->len)-8),inet_ntoa(ip->ip_src),(u_int16_t)ntohs(udp->source));
-	fprintf(dst, " --> %s:%d", inet_ntoa(ip->ip_dst),(u_int16_t)ntohs(udp->dest));
-	fprintf(dst, "\n");
-}
-
-static void print_icmp(FILE* dst, const struct ip* ip, const struct icmphdr* icmp){
-	fprintf(dst, "ICMP:\t %s ",inet_ntoa(ip->ip_src));
-	fprintf(dst, " --> %s ",inet_ntoa(ip->ip_dst));
-	fprintf(dst, "Type %d , code %d", icmp->type, icmp->code);
-	if( icmp->type==0 && icmp->code==0){
-		fprintf(dst, " echo reply: SEQNR = %d ", icmp->un.echo.sequence);
-	}
-	if( icmp->type==8 && icmp->code==0){
-		fprintf(dst, " echo reqest: SEQNR = %d ", icmp->un.echo.sequence);
-	}
-	fprintf(dst, "\n");
-}
-
-static void print_ipv4(FILE* dst, const struct ip* ip){
-	const void* payload = ((const char*)ip) + 4*ip->ip_hl;
-	fprintf(dst, "IPv4(HDR[%d])[", 4*ip->ip_hl);
-	fprintf(dst, "Len=%d:",(u_int16_t)ntohs(ip->ip_len));
-	fprintf(dst, "ID=%d:",(u_int16_t)ntohs(ip->ip_id));
-	fprintf(dst, "TTL=%d:",(u_int8_t)ip->ip_ttl);
-	fprintf(dst, "Chk=%d:",(u_int16_t)ntohs(ip->ip_sum));
-
-	if(ntohs(ip->ip_off) & IP_DF) {
-		fprintf(dst, "DF");
-	}
-	if(ntohs(ip->ip_off) & IP_MF) {
-		fprintf(dst, "MF");
-	}
-
-	fprintf(dst, " Tos:%0x]:\t",(u_int8_t)ip->ip_tos);
-
-	switch( ip->ip_p ) {
-	case IPPROTO_TCP:
-		print_tcp(dst, ip, (const struct tcphdr*)payload);
-		break;
-
-	case IPPROTO_UDP:
-		print_udp(dst, ip, (const struct udphdr*)payload);
-		break;
-
-	case IPPROTO_ICMP:
-		print_icmp(dst, ip, (const struct icmphdr*)payload);
-		break;
-
-	default:
-		fprintf(dst, "Unknown transport protocol: %d \n", ip->ip_p);
-		break;
-	}
-}
-
-static void print_eth(FILE* dst, const struct ethhdr* eth){
-	const void* payload = ((const char*)eth) + sizeof(struct ethhdr);
-	uint16_t h_proto = ntohs(eth->h_proto);
-	uint16_t vlan_tci;
-
-  begin:
-	switch ( h_proto ){
-	case ETHERTYPE_VLAN:
-		vlan_tci = ((const uint16_t*)payload)[0];
-		h_proto = ntohs(((const uint16_t*)payload)[0]);
-		payload = ((const char*)eth) + sizeof(struct ethhdr);
-		fprintf(dst, "802.1Q vlan# %d: ", 0x0FFF&ntohs(vlan_tci));
-		goto begin;
-
-	case ETHERTYPE_IP:
-		print_ipv4(dst, (const struct ip*)payload);
-		break;
-
-	case ETHERTYPE_IPV6:
-		printf("ipv6\n");
-		break;
-
-	case ETHERTYPE_ARP:
-		printf("arp\n");
-		break;
-
-	case 0x0810:
-		fprintf(dst, "MP packet\n");
-		break;
-
-	case STPBRIDGES:
-		fprintf(dst, "STP(0x%04x): (spanning-tree for bridges)\n", h_proto);
-		break;
-
-	case CDPVTP:
-		fprintf(dst, "CDP(0x%04x): (CISCO Discovery Protocol)\n", h_proto);
-		break;
-
-	default:
-		fprintf(dst, "Unknown ethernet protocol (0x%04x)\n", h_proto);
-		break;
-	}
-}
-
-static void print_packet(FILE* dst, cap_head* caphead){
-	static long pkt_counter = 0;
-
-	fprintf(dst, "[%04ld] ", ++pkt_counter);
-	print_eth(dst, caphead->ethhdr);
-}
-
 static int push_packet(struct CI* CI, write_head* whead, cap_head* head, unsigned char* packet_buffer){
 	const int recipient = filter(CI->iface, packet_buffer, head);
 	if ( recipient == -1 ){ /* no match */
@@ -189,7 +57,7 @@ static int push_packet(struct CI* CI, write_head* whead, cap_head* head, unsigne
 	}
 
 	if ( __builtin_expect(show_packets, 0) ){
-		print_packet(stderr, head);
+		format_pkg(stderr, &CI->format, head);
 	}
 
 	whead->consumer = recipient;
